16c519a37f031d9fbe52efc793757c1ce29bdd65
michalmelis <michalmelis.sk@gmail.com>
2019-10-08 22:29:09 +0200

    initial

---
+++ open-lab-01.1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace open_lab_01._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = Convert.ToInt32(Console.ReadLine());
+            if (a < 10)
+            {
+                Console.WriteLine("This number is less then 10");
+            }
+            else
+            {
+                Console.WriteLine("This number is greater or equal to 10");
+            }
+        }       
+    }
+}
+
+
+
